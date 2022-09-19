c8a7b1ba005e7594c244dcf26a0f66a32437334a
Daniel Vidal <daniel.vidal17@yahoo.com>
2022-09-19 12:37:00 -0400

    scheduler has been color coded based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
-var date = moment().format("dddd, MMMM Do")
+var timeBlocks = [
+    document.getElementById("08"),
+    document.getElementById("09"),
+    document.getElementById("10"),
+    document.getElementById("11"),
+    document.getElementById("12"),
+    document.getElementById("13"),
+    document.getElementById("14"),
+    document.getElementById("15"),
+    document.getElementById("16"),
+    document.getElementById("17")
+];
 
+var date = moment().format("dddd, MMMM Do")
+// console.log(timeBlocks[0].children[0].children[0].textContent);
 $("#currentDay").html(date);
+
+function checkTime(){
+    now = moment().format("HH")
+    for(var i=0; i<timeBlocks.length;i++){
+        if(timeBlocks[i].id<now){
+            timeBlocks[i].style.backgroundColor="grey";
+        }else if(timeBlocks[i].id == now){
+            timeBlocks[i].style.backgroundColor="red";
+        } else{
+            timeBlocks[i].style.backgroundColor="green";
+        }
+    }
+}
+checkTime();
